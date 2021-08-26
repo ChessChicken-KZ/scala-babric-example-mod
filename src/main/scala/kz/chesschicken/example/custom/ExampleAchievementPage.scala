dcbaa638ec2ddc7b874e7b0e521dd91d34e41695
ChessChicken-KZ <pool20net@mail.ru>
2021-08-26 07:24:47 +0600

    Redundant symbols.

--- src/main/scala/kz/chesschicken/example/custom/ExampleAchievementPage.scala
+++ src/main/scala/kz/chesschicken/example/custom/ExampleAchievementPage.scala
@@ -14,21 +14,21 @@ class ExampleAchievementPage(id : ModID, name: String) extends AchievementPage(i
     if (l <= 37 && row != 35) {
       if (l == 22) {
         if (random.nextInt(2) != 0)
-          k =  BlockBase.REDSTONE_ORE.texture
+          k = BlockBase.REDSTONE_ORE.texture
         else
           k = BlockBase.DIAMOND_ORE.texture
 
       } else if (l == 10) {
-        k = BlockBase.WOOD.texture;
+        k = BlockBase.WOOD.texture
       } else if (l == 8) {
-        k = BlockBase.REDSTONE_ORE.texture;
+        k = BlockBase.REDSTONE_ORE.texture
       } else if (l > 4) {
-        k = BlockBase.STONE.texture;
+        k = BlockBase.STONE.texture
       } else if (l > 0) {
-        k = BlockBase.DIRT.texture;
+        k = BlockBase.DIRT.texture
       }
     } else {
-      k = BlockBase.BEDROCK.texture;
+      k = BlockBase.BEDROCK.texture
     }
     k
   }
