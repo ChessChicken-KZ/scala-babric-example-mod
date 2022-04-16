dc738c65a807361f137eb699128652a30f46aaad
ChessChicken-KZ <53552480+ChessChicken-KZ@users.noreply.github.com>
2022-04-17 01:46:22 +0600

    Updated example mod, with Babric support.

--- src/main/scala/kz/chesschicken/example/events/init/TextureListener.scala
+++ src/main/scala/kz/chesschicken/example/events/init/TextureListener.scala
@@ -1,26 +1,19 @@
 package kz.chesschicken.example.events.init
 
-import kz.chesschicken.example.events.init.TextureListener.CUSTOM_MODEL
 import net.mine_diver.unsafeevents.listener.EventListener
 import net.modificationstation.stationapi.api.client.event.texture.TextureRegisterEvent
-import net.modificationstation.stationapi.api.client.model.json.JsonModel
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlases
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint
 import net.modificationstation.stationapi.api.registry.{Identifier, ModID}
 import net.modificationstation.stationapi.api.util.Null
-
-object TextureListener {
-  var CUSTOM_MODEL: JsonModel = _
-}
 
 class TextureListener {
   @Entrypoint.ModID
   var modID: ModID = Null.get()
 
   @EventListener def registerTextures(event: TextureRegisterEvent): Unit = {
-    ItemListener.coolItem.setTexture("/assets/examplemod/textures/item/coolItem.png")
+    ItemListener.coolItem.setTexture(Identifier.of(modID, "item/coolItem"))
     BlockListener.exampleBlock3.texture = Atlases.getStationTerrain.addTexture(Identifier.of(modID, "block/scotlalamsy")).index
-    CUSTOM_MODEL = JsonModel.get(Identifier.of(modID, "modelExample"))
   }
 
 }
--- src/main/scala/kz/chesschicken/example/wrappers/ExampleBlockWithModel.scala
+++ src/main/scala/kz/chesschicken/example/wrappers/ExampleBlockWithModel.scala
@@ -1,21 +1,12 @@
 package kz.chesschicken.example.wrappers
 
-import kz.chesschicken.example.events.init.TextureListener
 import net.minecraft.block.material.Material
-import net.minecraft.level.BlockView
-import net.modificationstation.stationapi.api.client.model.block.{BlockInventoryModelProvider, BlockWorldModelProvider}
-import net.modificationstation.stationapi.api.client.model.json.JsonModel
 import net.modificationstation.stationapi.api.registry.Identifier
 import net.modificationstation.stationapi.api.template.block.TemplateBlockBase
 
 
-class ExampleBlockWithModel(id: Identifier, m: Material) extends TemplateBlockBase(id, m) with BlockInventoryModelProvider with BlockWorldModelProvider {
+class ExampleBlockWithModel(id: Identifier, m: Material) extends TemplateBlockBase(id, m) {
 
   override def isFullOpaque: Boolean = false
-
-  override def getCustomWorldModel(level: BlockView, x: Int, y: Int, z: Int): JsonModel  = TextureListener.CUSTOM_MODEL
-
-  override def getInventoryModel(i: Int): JsonModel = TextureListener.CUSTOM_MODEL
-
 
 }
